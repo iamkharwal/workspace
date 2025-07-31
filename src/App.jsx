ff7ac753449b37ecf0536cdcff97d63fae9de460
ATul KhaRwal <93377463+iamkharwal@users.noreply.github.com>
2025-07-31 05:54:36 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Nested Drag and Drop</h1>
+      <h1>Hello Nested Drag and Drop </h1>
 
       <NestedListComponent />
     </div>
